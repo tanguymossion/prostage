4fcd6c5520ee5ad6dec911356f70ff6c95c1dc88
Tanguy Mossion <tmossion@iutbayonne.univ-pau.fr>
2019-11-19 13:39:14 +0100

    Les messages des pages sont affichés via Response

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProstageController extends AbstractController
 {
@@ -13,4 +14,25 @@ class ProstageController extends AbstractController
             'controller_name' => 'ProstageController',
         ]);
     }
+
+    public function bvn()
+    {
+        return new Response("<html><body><h1>Bienvenue sur la page d'accueil de Prostages</h1></body></html>");
+    }
+
+    public function entreprises()
+    {
+        return new Response("<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>");
+    }
+
+    public function formations()
+    {
+        return new Response("<html><body><h1>Cette page affichera la liste des formations de l'IUT</h1></body></html>");
+    }
+
+    public function stages($id)
+    {
+        return new Response("<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant $id</h1></body></html>");
+    }
+    
 }
