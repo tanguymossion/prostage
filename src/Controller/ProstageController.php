497e41e1599f5b0bd2a21609640d489941f1b1d0
Tanguy Mossion <tmossion@iutbayonne.univ-pau.fr>
2019-12-19 17:23:21 +0100

    Utilisation du mecanisme d'injection de dependances au niveau du controleur

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Stage;
 use App\Entity\Entreprise;
 use App\Entity\Formation;
+use App\Repository\StageRepository;
+use App\Repository\EntrepriseRepository;
+use App\Repository\FormationRepository;
 
 class ProstageController extends AbstractController
 {
@@ -18,11 +21,8 @@ class ProstageController extends AbstractController
         ]);
     }
 
-    public function bvn()
+    public function bvn(StageRepository $repStage)
     {
-        // Récupérer le repository de l'entité Stage
-        $repStage = $this->getDoctrine()->getRepository(Stage::class);
-        
         // Récupérer les stages enregistrés en BD
         $stages = $repStage->findAll();
 
@@ -30,11 +30,8 @@ class ProstageController extends AbstractController
         return $this->render('prostage/bvn.html.twig',['name' => 'Accueil','stages' => $stages]);
     }
 
-    public function entreprises()
+    public function entreprises(EntrepriseRepository $repEntreprise)
     {
-        // Récupérer le repository de l'entité Entreprise
-        $repEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
-        
         // Récupérer les entreprises enregistrées en BD
         $entreprises = $repEntreprise->findAll();
 
@@ -42,11 +39,8 @@ class ProstageController extends AbstractController
         return $this->render('prostage/entreprises.html.twig',['name' => 'Entreprises','entreprises' => $entreprises]);
     }
 
-    public function formations()
+    public function formations(FormationRepository $repFormation)
     {
-        // Récupérer le repository de l'entité Formation
-        $repFormation = $this->getDoctrine()->getRepository(Formation::class);
-        
         // Récupérer les entreprises enregistrées en BD
         $formations = $repFormation->findAll();
 
@@ -54,38 +48,20 @@ class ProstageController extends AbstractController
         return $this->render('prostage/formations.html.twig',['name' => 'Formations','formations' => $formations]);
     }
 
-    public function stages($id)
+    public function stages(Stage $stage)
     {
-        // Récupérer le repository de l'entité Stage
-        $repStage = $this->getDoctrine()->getRepository(Stage::class);
-        
-        // Récupérer le stage correspondant à l'id enregistré en BD
-        $stage = $repStage->find($id);
-
         // Envoyer le stage récupéré à la vue
         return $this->render('prostage/stages.html.twig',['name' => 'Stage','stage' => $stage]);
     }
     
-    public function parEntreprise($id)
+    public function parEntreprise(Entreprise $entreprise)
     {
-        // Récupérer le repository de l'entité Entreprise
-        $repEntreprise = $this->getDoctrine()->getRepository(Entreprise::class);
-        
-        // Récupérer l'entreprise correspondant à l'id enregistré en BD
-        $entreprise = $repEntreprise->find($id);
-
         // Envoyer l'entreprise récupérée à la vue
         return $this->render('prostage/parEntreprise.html.twig',['name' => 'Stages par entreprise','entreprise' => $entreprise]);
     }
 
-    public function parFormation($id)
+    public function parFormation(Formation $formation)
     {
-        // Récupérer le repository de l'entité Formation
-        $repFormation = $this->getDoctrine()->getRepository(Formation::class);
-        
-        // Récupérer la formation correspondant à l'id enregistré en BD
-        $formation = $repFormation->find($id);
-
         // Envoyer la formation récupérée à la vue
         return $this->render('prostage/parFormation.html.twig',['name' => 'Stages par formation','formation' => $formation]);
     }
