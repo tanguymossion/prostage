5f3476f16aa2612425e1d5b976d15753428a6551
Tanguy Mossion <tmossion@iutbayonne.univ-pau.fr>
2020-03-02 11:59:54 +0100

    Ajout des fonctions de connexion - deconnexion dans la vue

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -29,8 +29,8 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    /*public function logout()
+    public function logout()
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
-    }*/
+
+    }
 }
